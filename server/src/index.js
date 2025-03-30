89da6059b889b86dc9cc12d2649e73f599706eb0
codes-harshit <programming.harshit44@gmail.com>
2025-03-30 23:48:00 +0530

    update cors

--- server/src/index.js
+++ server/src/index.js
@@ -14,7 +14,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://chatapp-frontend-r9c1.onrender.com",
     credentials: true,
   })
 );
